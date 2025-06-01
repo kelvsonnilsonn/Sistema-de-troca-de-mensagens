d36449893a2f9e4fe087d3b056a968d75057a5a3
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-06-01 16:28:05 -0300

    Novos métodos.

--- src/main/java/com/orizon/system/message/repositories/MessageRepository.java
+++ src/main/java/com/orizon/system/message/repositories/MessageRepository.java
@@ -3,5 +3,10 @@ package com.orizon.system.message.repositories;
 import com.orizon.system.message.domain.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    Optional<List<Message>> findByReceiverId(Long receiverId);
+    Optional<List<Message>> findBySenderId(Long senderId);
 }
